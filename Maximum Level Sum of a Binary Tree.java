f6a89f74a126505fb3ef18d7109a9073f3713031
Annie Valentina A <Annievalentina1307@gmail.com>
2026-01-06 21:42:31 +0530

    Add solution for maximum level sum of binary tree
    Implement a solution to find the level with the maximum sum in a binary tree.

---
+++ Maximum Level Sum of a Binary Tree.java
@@ -0,0 +1,64 @@
+/*
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+Example 2:
+
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-105 <= Node.val <= 105
+
+*/
+
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        int level = 1;
+        int ansLevel = 1;
+        int maxSum = Integer.MIN_VALUE;
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int sum = 0;
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            
+            // Check if this level has max sum
+            if (sum > maxSum) {
+                maxSum = sum;
+                ansLevel = level;
+            }
+            
+            level++;
+        }
+        
+        return ansLevel;
+    }
+}
